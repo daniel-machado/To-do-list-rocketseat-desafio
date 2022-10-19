433d46a2871d676e7e8ac025a249832df4299f10
Barbosa <daniel.barbosa@capgemini.com>
2022-10-19 10:13:03 -0300

    adicao de edicao e alerta

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,22 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const findTask = tasks.find((task) => task.title === newTaskTitle);
     const data: Task = {
       id: new Date().getTime(),
       title: newTaskTitle,
       done: false,
     };
 
-    setTasks(oldState => [...oldState, data]);
+    if (!findTask) {
+      setTasks(oldState => [...oldState, data]);
+      return;
+    }
+    Alert.alert(
+      "Task já cadastrada",
+      "Você não pode cadastrar uma task com o mesmo nome"
+    );
+    return; 
   }
 
   function handleToggleTaskDone(id: number) {
@@ -26,7 +35,6 @@ export function Home() {
       }
       return task;
     }))
-    //settaskValidationDone(done => !done)
   }
 
   function handleRemoveTask(id: number) {
@@ -45,6 +53,29 @@ export function Home() {
     );
   }
 
+  function handleEditTask(taskId: number, taskNewTitle: string ){
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId){
+        task = {
+          ...task,
+          title: taskNewTitle,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+    /*setTasks(prevState => prevState.map(task => {
+      if(task.id === taskId){
+        task = {
+          ...task,
+          title: taskNewTitle,
+        };
+      }
+      return task
+    }));
+    */
+  }
+
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
@@ -55,6 +86,7 @@ export function Home() {
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
+        editTask={handleEditTask}
       />
     </View>
   )
